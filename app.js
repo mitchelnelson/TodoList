a7345b392475c5df9498d3d905ca37da42bc383e
Mitchel Nelson <66894405+mitchelnelson@users.noreply.github.com>
2021-04-20 17:42:55 -0600

    added buttons to each item

--- app.js
+++ app.js
@@ -6,13 +6,25 @@ itemButton.addEventListener('click', addListItem);
 
 function addListItem (evt) {
 	let listItem = document.createElement('div');
-	listItem.innerText = userEntry.value;
-	listItem.classList.add('item-style');
+	listItem.innertext = userEntry.value;
+	addListItemButtons(listItem);
 	listArea.appendChild(listItem);
-	clearTextField();
+	userEntry.value = '';
 	userEntry.focus();
 }
 
-function clearTextField () {
-	userEntry.value = '';
+function addListItemButtons (item) {
+	let completeButton = document.createElement('button');
+	let editButton = document.createElement('button');
+	let deleteButton = document.createElement('button');
+
+	completeButton.innerText = 'Completed';
+	editButton.innerText = 'Edit';
+	deleteButton.innerText = 'Delete';
+
+	item.appendChild(completeButton);
+	item.appendChild(editButton);
+	item.appendChild(deleteButton);
 }
+
+// listItem.classList.add('item-style');
