21c2d249893d13fe6bcc1baadff39d5f2b320304
Mitchel Nelson <66894405+mitchelnelson@users.noreply.github.com>
2021-04-21 17:07:47 -0600

    allows user to change item name on the fly

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const userEntry = document.getElementById('user-entry');
 const itemButton = document.getElementById('item-button');
 const listArea = document.querySelector('.content-main');
+let itemSibling = document.querySelector('.content-main div');
+
+let itemCount = 0;
 
 itemButton.addEventListener('click', addListItem);
 
 function addListItem (evt) {
 	let listItem = document.createElement('div');
-	listItem.innertext = userEntry.value;
+	listItem.setAttribute('class', `item-${itemCount}`);
+	listItem.innerText = userEntry.value;
+
+	itemCount++;
+
 	addListItemButtons(listItem);
 	listArea.appendChild(listItem);
 	userEntry.value = '';
 	userEntry.focus();
+
+	itemSibling = itemSibling.nextElementSibling;
+	editItemListener(itemSibling);
 }
 
 function addListItemButtons (item) {
@@ -27,4 +37,45 @@ function addListItemButtons (item) {
 	item.appendChild(deleteButton);
 }
 
-// listItem.classList.add('item-style');
+function editItemListener (item) {
+	item.childNodes[2].addEventListener('click', () => {
+		// Save initial text entry; disable the complete button; rename the edit button;
+		let initialText = item.childNodes[0].textContent;
+		item.childNodes[1].setAttribute('disabled', true);
+		item.childNodes[2].innerText = 'Save';
+
+		// Create new input box
+		let flyInput = document.createElement('input');
+		flyInput.setAttribute('id', 'fly-input');
+		flyInput.setAttribute('value', initialText);
+
+		// Remove the previous text for the item and replace it with an input field
+		item.removeChild(item.childNodes[0]);
+		item.prepend(flyInput);
+
+		// autofocus and set cursor to the end of the previous text
+		document.getElementById('fly-input').focus();
+		document
+			.getElementById('fly-input')
+			.setSelectionRange(initialText.length, initialText.length);
+
+		// add new event listener to save the new input
+		item.childNodes[2].addEventListener('click', () => {
+			if (flyInput.value) {
+				// store the user's new input in a variable, remove the input field, and replace text
+				let newText = flyInput.value;
+				item.removeChild(item.childNodes[0]);
+				item.prepend(newText);
+
+				// Restore the buttons and listen for edits again (i.e. save)
+				item.childNodes[1].removeAttribute('disabled');
+				item.childNodes[2].innerText = 'Edit';
+				editItemListener(item);
+			}
+		});
+	});
+}
+
+// function styleItem () {
+// 	listItem.classList.add('item-style');
+// }
